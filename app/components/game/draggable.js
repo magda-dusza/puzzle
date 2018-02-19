1e86437584ded714e98425a6eb72a3b70ad311ae
Magda <magdalena.dusza13@gmail.com>
2018-02-19 23:27:14 +0100

    split picture, add simple drag&drop

---
+++ app/components/game/draggable.js
@@ -0,0 +1,33 @@
+/* @ngInject */
+export default function draggable($document) {
+
+  return function drag(scope, element, attr) {
+    var startX = 0, startY = 0, x = 0, y = 0;
+    element.css({
+      position: 'relative',
+      cursor: 'pointer'
+    });
+    element.on('mousedown', (event) => {
+      // Prevent default dragging of selected content
+      event.preventDefault();
+      startX = event.screenX - x;
+      startY = event.screenY - y;
+      $document.on('mousemove', mousemove);
+      $document.on('mouseup', mouseup);
+    });
+
+    function mousemove(event) {
+      y = event.screenY - startY;
+      x = event.screenX - startX;
+      element.css({
+        top: y + 'px',
+        left:  x + 'px'
+      });
+    }
+
+    function mouseup() {
+      $document.off('mousemove', mousemove);
+      $document.off('mouseup', mouseup);
+    }
+  };
+}
--- app/components/game/game.module.js
+++ app/components/game/game.module.js
@@ -2,11 +2,15 @@ import angular from 'angular';
 import routing from './game.route';
 import component from './game.component';
 import service from './game.service';
+import draggable from './draggable.js';
 /* @ngInject */
+
+import './game.scss';
 
 let game = angular.module('game', [])
   .component('game', component)
   .service('gameService', service)
+  .directive('ciDraggable', draggable)
   .config(routing)
   .name;
 
