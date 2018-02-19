216bdf66ee8dd4da520fcf639f820b876de33900
Magda <magdalena.dusza13@gmail.com>
2018-02-19 20:31:09 +0100

    init styles

--- app/app.module.js
+++ app/app.module.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import example from './example/example.module';
 
-require('./main.scss');
+require('../assets/styles/main.scss');
 angular.module('app', [
   uirouter,
   'example'
